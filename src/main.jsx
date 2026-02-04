391287513e2889c844f323afd6029c2be40e017f
Konny Daugh <natalydiach@gmail.com>
2026-02-04 10:00:06 +0100

    Set up base project architecture

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import RGSchoolApp from './RGSchoolApp.jsx'
+import RGSchoolApp from './app/RGSchoolApp.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
