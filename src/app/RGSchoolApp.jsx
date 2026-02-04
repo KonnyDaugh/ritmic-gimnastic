03cd87245ea925a211eb10ef8e6db6d7916d4443
Konny Daugh <natalydiach@gmail.com>
2026-02-04 10:35:29 +0100

    Add SchoolAbout page with school info and coaches

--- src/app/RGSchoolApp.jsx
+++ src/app/RGSchoolApp.jsx
@@ -1,11 +1,13 @@
 import './RGSchoolApp.css';
 import GymnastList from '../components/GymnastList';
+import SchoolAbout from '../pages/SchoolAbout';
 
 function RGSchoolApp() {
 
   return (
     <div>
         <h1>RG School App</h1>
+        <SchoolAbout />
         <GymnastList />
     </div>
   )
---
+++ src/pages/SchoolAbout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {schoolInfo} from '../data/school';
+
+function SchoolAbout() {
+    return (
+        <div>
+            <h1>{schoolInfo.name}</h1>
+            <p>{schoolInfo.description}</p>
+            <p>Основана: {schoolInfo.established} год</p>
+            <h2>Тренеры</h2>
+            <ul>
+                {schoolInfo.coaches.map(coach => (
+                    <li>
+                        {coach.name} - опыт {coach.experience} лет
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default SchoolAbout;
